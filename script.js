021d5ab6d78daad717bbcaef90d77580c27cf25d
Vsnikhilmaheswar <84986865+Vsnikhilmaheswar@users.noreply.github.com>
2024-09-11 22:54:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-getWeatherData = (city) =>{
-    const url = `https://open-weather13.p.rapidapi.com/city/${city}/EN`;
-    return url
-}
-
-
-
-const options = {
+getWeatherData = (city) => {
+  const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': 'ac8bb0b40cmsh88b996c0dc37bcfp12afa7jsnf89b095f7430',
-		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
-url=getWeatherData("landon")
-console.log(url,)
-fetch(url, options)
+return fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`, options)
 .then(response =>response.json())
-.then(data =>console.log(data))
+.then(data => data)
+.catch(err => console.log(err))
+}
+const  searchCity = async() => {
+  const city = document.getElementById('city-input').value;
+  console.log(city)
+ const data = await getWeatherData(city)
+ console.log(data)
+ showWeatherData(data)
+}
+const showWeatherData = (weatherData) => {
+ console.log("ss",weatherData)
+  document.getElementById('temp').innerText =weatherData.current.temp_c
+  document.getElementById('city-name').innerText =weatherData.location.name
+  document.getElementById('weather-type').innerText =weatherData.current.condition.text
+  document.getElementById('wind_kph').innerText =weatherData.current.wind_kph
+  document.getElementById('Humidity').innerText =weatherData.current.humidity
+}
+
